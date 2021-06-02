e95fc61578f0c3dd0de5e9148e498ad331f48000
martin-garcia-blanco <programadopormartin@gmail.com>
2021-06-02 13:03:49 +0200

    Add vote link behaviour

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,8 +1,65 @@
-const Link = ({ link }) => {
+import { AUTH_TOKEN, LINKS_PER_PAGE } from "../constants";
+import { gql, useMutation } from "@apollo/client";
+import { FEED_QUERY } from "./LinkList";
+
+const VOTE_MUTATION = gql`
+  mutation VOTE_MUTATION($linkId: ID!) {
+    vote(linkId: $linkId) {
+      id
+      user {
+        id
+      }
+    }
+  }
+`;
+
+const Link = ({ link, index }) => {
+  const authToken = localStorage.getItem(AUTH_TOKEN);
+  const [vote] = useMutation(VOTE_MUTATION, {
+    variables: { linkId: link.id },
+    update(cache, { data: { vote } }) {
+      const { feed } = cache.readQuery({ query: FEED_QUERY });
+      const updatedLinks = feed.map((feedLink) => {
+        if (feedLink.id === link.id) {
+          return {
+            ...feedLink,
+            votes: [...feedLink.votes, vote],
+          };
+        }
+        return feedLink;
+      });
+      cache.writeQuery({ query: FEED_QUERY, data: { feed: updatedLinks } });
+    },
+  });
+
+  const take = LINKS_PER_PAGE;
+  const skip = 0;
+
   return (
-    <div>
-      <div>
-        {link.description} ({link.url})
+    <div className="flex mt2 items-start">
+      <div className="flex items-center">
+        <span className="gray">{index + 1}.</span>
+        {authToken && (
+          <div
+            className="ml1 gray f11"
+            style={{ cursor: "pointer" }}
+            onClick={vote}
+          >
+            ▲
+          </div>
+        )}
+      </div>
+      <div className="ml1">
+        <div>
+          {link.description} ({link.url})
+        </div>
+        {authToken && (
+          <div className="f6 lh-copy gray">
+            {link.votes.length} votes | by{" "}
+            {link.postedBy ? link.postedBy.name : "Unknown"}{" "}
+            {/* {timeDifferenceForDate(link.createdAt)} */}
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,12 +1,18 @@
 import Link from "./Link";
 import { gql, useQuery } from "@apollo/client";
 
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
   {
     feed {
       id
       url
       description
+      postedBy {
+        name
+      }
+      votes {
+        id
+      }
     }
   }
 `;
@@ -15,7 +21,10 @@ const LinkList = () => {
   const { data, loading, error } = useQuery(FEED_QUERY);
   return (
     <div>
-      {data && data.feed.map((link) => <Link link={link} key={link.id} />)}
+      {data &&
+        data.feed.map((link, index) => (
+          <Link link={link} key={link.id} index={index} />
+        ))}
     </div>
   );
 };
