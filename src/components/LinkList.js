c4d46b3fa8e98df58a683fafff3202f042a7a950
martin-garcia-blanco <programadopormartin@gmail.com>
2021-06-02 10:37:04 +0200

    Fetch with gql and display links

---
+++ src/components/LinkList.js
@@ -0,0 +1,23 @@
+import Link from "./Link";
+import { gql, useQuery } from "@apollo/client";
+
+const FEED_QUERY = gql`
+  {
+    feed {
+      id
+      url
+      description
+    }
+  }
+`;
+
+const LinkList = () => {
+  const { data, loading, error } = useQuery(FEED_QUERY);
+  console.log(data, loading, error);
+  return (
+    <div>
+      {data && data.feed.map((link) => <Link link={link} key={link.id} />)}
+    </div>
+  );
+};
+export default LinkList;
